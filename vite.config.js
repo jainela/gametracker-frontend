98a1c4b8d7b1e4571d93543189c04a1ab77d8729
jainela <Jainelamartinez806@gmail.com>
2025-11-22 18:30:56 -0500

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/gametracker-frontend/',
-   plugins: [react()],
-
+  
   plugins: [
+    [react()],
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
