48c0653ab22d126fd9d55fe25dcc9065160c9370
jainela <Jainelamartinez806@gmail.com>
2025-11-22 18:44:52 -0500

    actualizaciones

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'gametracker-frontend',
+  base: '/gametracker-frontend/',
   
   plugins: [
-    [react()],
+    
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
